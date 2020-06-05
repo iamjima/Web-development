0c857669d2491d7dcbef21fb0d4206bf59cc71ab
iamjima <45260265+iamjima@users.noreply.github.com>
2020-06-05 11:13:37 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+// Creating variables
+var firstName;
+let lastName;
+
+// Assinging Values
+var firstName = "Jithma";
+let lastName = "Pramudith";
+
+// create a function
+
+function myFunction() {
+
+}
+
+// call a function
+
+myFunction();
